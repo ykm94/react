f69b0511e57ba9c51208c403273ab0dc8b02a7d7
ykm11 <jjpig92@naver.com>
2022-09-20 21:03:42 +0900

    first commit

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>1234</div>
+  )
+}
+
+export default Home
